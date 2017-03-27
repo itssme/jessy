5b85ed0f4befaf4fd23dd9d941faa2a2f185eae0
joel <itssme3000@gmail.com>
2017-03-27 12:16:15 +0200

    Updated Move

--- src/board/Move.java
+++ src/board/Move.java
@@ -1,6 +1,7 @@
 package board;
 
 import chessfigure.ChessFigure;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -13,10 +14,12 @@ import org.json.JSONObject;
 public class Move {
 
     private ChessFigure chessFigure;
+    private Position from;
     private Position to;
 
-    public Move(ChessFigure chessFigure, Position to) {
+    public Move(ChessFigure chessFigure, Position from,Position to) {
         this.chessFigure = chessFigure;
+        this.from = from;
         this.to = to;
     }
 
@@ -24,11 +27,26 @@ public class Move {
         return chessFigure;
     }
 
+    public Position getFrom() { return from; }
+
     public Position getTo() {
         return to;
     }
 
     public JSONObject toJsonObject() {
-        return to.toJsonObject();
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put("from x", from.getX());
+            obj.put("from y", from.getY());
+
+            obj.put("to x", to.getX());
+            obj.put("to y", to.getY());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
     }
 }

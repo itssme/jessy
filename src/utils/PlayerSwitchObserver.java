f5282b8b6c43fd711a6dd5aab06bae2bc75fe7e9
Simon Königsreiter <simon.koenigsreiter@gmail.com>
2017-05-19 10:23:56 +0200

    Added two models for Game and Player. I also added some static Utilities which keeps track of which player's turn it is. The PlayerSwitchObserver interface is a simple Observer who gets called once the player switches.

---
+++ src/utils/PlayerSwitchObserver.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Author:  Königsreiter Simon
+ * Class:   3CHIF
+ * Date:    19.05.17
+ * Poject:  jessy
+ * Package: utils
+ * Desc.:
+ */
+public interface PlayerSwitchObserver {
+
+    void onPlayerSwitch(final boolean canPlay);
+
+}
